bcc6828238ddcfb887b85349b1db6c484e0f4750
JanMouwes <jan.mouwes@gmail.com>
2026-04-01 17:23:38 +0200

    implement day 02

--- src/main/scala/aoc2024/day02/Day02.scala
+++ src/main/scala/aoc2024/day02/Day02.scala
@@ -1,38 +1,93 @@
 package aoc2024.day02
 
-
 def solvePart1(input: String): String = {
-  val (left, right) = getLists(input)
-  val zipped = left.sorted.zip(right.sorted)
-  val doFold = (sum: Int, tup: (Int, Int)) => sum + (tup._1 - tup._2).abs
-  val diff: Int = zipped.foldLeft(0)(doFold)
+  val reports = parseReports(input)
+  val diff: Int = reports.count(isSafe)
 
   diff.toString
 }
 
 def solvePart2(input: String): String = {
-  val (left, right) = getLists(input)
-  val appearances = countOccurrences(right)
+  val reports = parseReports(input)
+  val diff: Int = reports.count(isSafeWithDampener)
 
-  val score = left.foldLeft(0)((acc, l) => acc + l * appearances(l))
-
-  score.toString
+  diff.toString
 }
 
-def getLists(input: String): (Array[Int], Array[Int]) = {
+
+type Report = IndexedSeq[Int]
+
+def parseReports(input: String): Seq[Report] = {
   val lines = input.split('\n').filter(s => s != "")
-  val tuples = lines.map(l => {
+  val reports = lines.map(l => {
     val splitLine = l.split(' ')
     val nums = splitLine.filter(s => s != "").map(s => s.toInt)
 
-    (nums(0), nums(1))
+    nums.toIndexedSeq
   })
-
-  (tuples.map(t => t._1), tuples.map(t => t._2))
+  reports.toSeq
 }
 
-def countOccurrences[T](list: Array[T])(implicit ordering: Ordering[T]): Map[T, Int] = {
-  list.foldLeft(Map[T, Int]().withDefaultValue(0)) {
-    case (acc, item) => acc + (item -> (1 + acc(item)))
+
+private def isSafe(report: Report): Boolean = {
+  val diffWindows = report.sliding(2).map { case Seq(n1: Int, n2: Int) => n2 - n1 }.toSeq
+  val isAscending = diffWindows.forall(n => n > 0)
+  val isDescending = diffWindows.forall(n => n < 0)
+  val safeStepThreshold = 3
+  val isSmallStep = diffWindows.forall(diff => diff.abs <= safeStepThreshold)
+
+  (isAscending || isDescending) && isSmallStep
+}
+
+def bruteForce(reports: Seq[Report]): Seq[Report] = {
+  reports.filter(computeReportVariants(_).exists(isSafe))
+}
+
+private def isSafeWithDampener(report: Report): Boolean = {
+  isSafe(report) || isFixable(report)
+}
+
+def isFixable(report: Report): Boolean = {
+  isSafe(report.drop(1)) ||
+    isSafe(report.dropRight(1)) ||
+    findViolations(report).exists(i => {
+      isSafe(report.patch(i, Nil, 1))
+    })
+}
+
+/**
+ * @return indexes of safety violations
+ */
+def findViolations(report: Report): Seq[Int] = {
+  val safeStepThreshold = 3
+
+  def isStepTooBig(n1: Int, n2: Int) = (n2 - n1).abs > safeStepThreshold
+
+  val maybeHeadIndex: Option[Int] = {
+    report.take(2) match {
+      case IndexedSeq(n1: Int, n2: Int) if isStepTooBig(n1, n2) => Some(0)
+      case _ => None
+    }
   }
+  val maybeTailIndex: Option[Int] = {
+    report.takeRight(2) match {
+      case IndexedSeq(n1: Int, n2: Int) if isStepTooBig(n1, n2) => Some(0)
+      case _ => None
+    }
+  }
+
+  maybeHeadIndex.concat(
+    report.sliding(3).zipWithIndex.filter { case (Seq(n1: Int, n2: Int, n3: Int), i: Int) =>
+      val shouldAscend = n3 > n1
+      val isViolation = shouldAscend == n1 > n2 || shouldAscend == n2 > n3 || n2 == n1 || isStepTooBig(n1, n2)
+
+      isViolation
+    }.map(_._2 + 1).concat(maybeTailIndex)
+  ).toSeq
+}
+
+def computeReportVariants(report: Report): Seq[Report] = {
+  report.zipWithIndex.map((n, i) => {
+    report.patch(i, Nil, 1)
+  })
 }
