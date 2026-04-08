8dab09958f2f374b33e3df12854599d65d111c73
JanMouwes <jan.mouwes@gmail.com>
2026-04-08 16:02:29 +0200

    rework test into parameterised test, add AoC example

--- src/test/scala/aoc2024/day08/MapParserSpec.scala
+++ src/test/scala/aoc2024/day08/MapParserSpec.scala
@@ -2,37 +2,60 @@ package aoc2024.day08
 
 import aoc2024.base.AocSpecBase
 import aoc2024.day06.{Coordinate, Dimensions}
+import org.scalatest.prop.TableDrivenPropertyChecks.forAll
+import org.scalatest.prop.Tables.Table
 
 class MapParserSpec extends AocSpecBase {
 
   behavior of "parseMap"
 
-  it should "parse a small empty map" in {
-    val input = "..."
-
-    val maybeActual = parseMap(input)
-
-    maybeActual should be(defined)
-    val actual = maybeActual.get
-    actual.dimensions should equal(Dimensions(3, 1))
-    actual.antennas should be(empty)
-  }
-
-  it should "yield antennas" in {
-    val input =
+  private val cases = Table(
+    ("map", "dimensions", "antennas"),
+    ("...", Dimensions(3, 1), Set.empty),
+    (
       """x.o
         |...
-        |...""".stripMargin
+        |...""".stripMargin,
+      Dimensions(3, 3),
+      Set(('x', Coordinate(0, 0)), ('o', Coordinate(2, 0)))
+    ),
+    (
+      """............
+        |........0...
+        |.....0......
+        |.......0....
+        |....0.......
+        |......A.....
+        |............
+        |............
+        |........A...
+        |.........A..
+        |............
+        |............""".stripMargin,
+      Dimensions(12, 12),
+      Set(
+        ('0', Coordinate(8, 1)),
+        ('0', Coordinate(5, 2)),
+        ('0', Coordinate(7, 3)),
+        ('0', Coordinate(4, 4)),
+        ('A', Coordinate(6, 5)),
+        ('A', Coordinate(8, 8)),
+        ('A', Coordinate(9, 9)),
+      ),
+    )
+  )
 
-    val maybeActual = parseMap(input)
+  it should "parse the map correctly in described cases" in {
+    forAll(cases) {
+      (input, expectedDimensions, expectedAntennas) => {
+        val maybeActual = parseMap(input)
 
-    maybeActual should be(defined)
-    val actual = maybeActual.get
-    actual.dimensions should equal(Dimensions(3, 3))
-    actual.antennas should equal(Set(
-      ('x', Coordinate(0, 0)),
-      ('o', Coordinate(2, 0))
-    ))
+        maybeActual should be (defined)
+        val actual = maybeActual.get
+        actual.dimensions should equal(expectedDimensions)
+        actual.antennas should equal(expectedAntennas)
+      }
+    }
   }
 
   it should "crash on an empty string" in {
@@ -40,6 +63,6 @@ class MapParserSpec extends AocSpecBase {
 
     val maybeActual = parseMap(input)
 
-    maybeActual should not be(defined)
+    maybeActual should not be defined
   }
 }
