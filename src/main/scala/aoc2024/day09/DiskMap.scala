3d9c2bb71241b7952f888ebc35ac07d105fb9ffe
JanMouwes <jan.mouwes@gmail.com>
2026-04-21 14:04:00 +0200

    implement DiskMap compression

--- src/main/scala/aoc2024/day09/DiskMap.scala
+++ src/main/scala/aoc2024/day09/DiskMap.scala
@@ -1,9 +1,78 @@
 package aoc2024.day09
 
-case class DiskMap(blocks: Seq[Int])
+import scala.annotation.tailrec
+
+case class DiskMap(blocks: Seq[DiskBlock]) {
+  def files: Seq[DiskBlock.File] = {
+    blocks.collect { case f: DiskBlock.File => f }
+  }
+
+  def gaps: Seq[DiskBlock.Gap] = {
+    blocks.collect { case f: DiskBlock.Gap => f }
+  }
+
+  def totalFileSize: Int = {
+    this.files.map(_.size).sum
+  }
+}
+
+enum DiskBlock(val size: Int):
+  case File(id: Int, override val size: Int) extends DiskBlock(size)
+  case Gap(override val size: Int) extends DiskBlock(size)
 
 def parseDiskMap(diskMap: String): DiskMap = {
   val ints = diskMap.toSeq.map(_.asDigit)
-  
-  DiskMap(ints)
+
+  val blocks = ints.zipWithIndex.map((int, index) => {
+    val isEven = index % 2 == 0
+    val id = index / 2
+    if isEven then DiskBlock.File(id, int) else DiskBlock.Gap(int)
+  })
+
+  DiskMap(blocks)
+}
+
+@tailrec
+def compressDiskMap(diskMap: DiskMap): DiskMap = {
+  if (diskMap.gaps.isEmpty) return diskMap
+
+  val file = diskMap.files.last
+  val gap = diskMap.gaps.head
+
+  def doCompressionStep(diskMap: DiskMap): DiskMap = {
+    val remaining = gap.size - file.size
+    val (leftOfFirstGap, firstGapAndRest) = diskMap.blocks.span(_ != gap)
+
+    val constructedTail = if remaining < 0
+    then {
+      val fileLeftPart = DiskBlock.File(file.id, gap.size)
+      val fileRightPart = DiskBlock.File(file.id, -remaining)
+
+      val withoutGapAndFile = firstGapAndRest.tail.init
+
+      val constructedTail =
+        if withoutGapAndFile.nonEmpty
+        then fileLeftPart +: withoutGapAndFile :+ fileRightPart
+        else Seq(file)
+
+      constructedTail
+    }
+    else {
+      val newGap = DiskBlock.Gap(remaining)
+
+      val constructedTail = file +: newGap +: firstGapAndRest.tail.init
+
+      constructedTail
+    }
+
+    val tripEndGaps = (leftOfFirstGap ++ constructedTail).reverse.dropWhile(_.isInstanceOf[DiskBlock.Gap]).reverse
+    
+    DiskMap(tripEndGaps)
+  }
+
+  val result = doCompressionStep(diskMap)
+
+  if result == diskMap
+  then diskMap
+  else compressDiskMap(result)
 }
--- src/test/scala/aoc2024/day09/DiskMapSpec.scala
+++ src/test/scala/aoc2024/day09/DiskMapSpec.scala
@@ -1,6 +1,9 @@
 package aoc2024.day09
 
 import aoc2024.base.AocSpecBase
+import aoc2024.day09.DiskBlock.{File, Gap}
+import org.scalatest.prop.TableDrivenPropertyChecks.forAll
+import org.scalatest.prop.Tables.Table
 
 class DiskMapSpec extends AocSpecBase {
   behavior of "parseDiskMap"
@@ -16,10 +19,48 @@ class DiskMapSpec extends AocSpecBase {
 
   it should "parse the expected input" in {
     val input = "123"
-    val expected = DiskMap(Seq(1, 2, 3))
+    val expected = DiskMap(Seq(File(0, 1), Gap(2), File(1, 3)))
 
     val actual = parseDiskMap(input)
 
     actual should equal(expected)
+  }
+
+  behavior of "compressDiskMap"
+
+  it should "retain total file size" in {
+    val input = DiskMap(Seq(File(0, 1), Gap(2), File(1, 3)))
+
+    val actual = compressDiskMap(input)
+
+    actual.totalFileSize should equal(input.totalFileSize)
+  }
+
+  it should "produce the expected result" in {
+    val cases = Table(
+      ("input", "expected"),
+      (
+        DiskMap(Seq(File(0, 1), Gap(2), File(1, 3))),
+        DiskMap(Seq(File(0, 1), File(1, 3)))
+      ),
+      (
+        DiskMap(Seq(File(0, 1), Gap(4), File(1, 3))),
+        DiskMap(Seq(File(0, 1), File(1, 3)))
+      ),
+      (
+        DiskMap(Seq(File(0, 1), Gap(1), File(1, 3), Gap(2), File(2, 3))),
+        DiskMap(Seq(File(0, 1), File(2, 1), File(1, 3), File(2, 2)))
+      )
+
+    )
+
+    forAll(cases) {
+      (input, expected) => {
+       
+        val actual = compressDiskMap(input)
+
+        actual should equal(expected)
+      }
+    }
   }
 }
