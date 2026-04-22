503be19c29941535ebffc9a70a72d22c836f4bb3
JanMouwes <jan.mouwes@gmail.com>
2026-04-22 13:08:39 +0200

    wire together day 09 part 2

--- src/main/scala/aoc2024/day09/Day09.scala
+++ src/main/scala/aoc2024/day09/Day09.scala
@@ -2,14 +2,16 @@ package aoc2024.day09
 
 def solvePart1(input: String): String = {
   val diskMap = DiskMap.parse(input)
-  
-//  println("given:\n" + diskMap.render)
-  
+
   val compressed = compressDiskMap(diskMap)
   
   compressed.checksum.toString
 }
 
 def solvePart2(input: String): String = {
-  ""
+  val diskMap = DiskMap.parse(input)
+
+  val defragmented = defragmentDiskMap(diskMap)
+
+  defragmented.checksum.toString
 }
